b58d48385ad5ba4ccef1cb32dc0ac188f5a493ca
abdo ahmed <146590912+abdoelhashem@users.noreply.github.com>
2023-10-18 16:17:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -21,19 +21,12 @@ if (document.querySelector(".sms").value != "") {
             clearInterval(time);
         }
     }, 1000);
-        var prams = {
-            name: "saraha",
-            email: "asmaahmd7x@gmail.com",
-            message: document.querySelector(".sms").value,
-        };
-        let service_id = "service_cb87tb5";
-        let template_id = "template_ml3cair";
-        emailjs.send(
-            service_id, template_id, prams
-        ).then(res => {
-            document.querySelector(".sms").value = "";
-            document.querySelector(".errs").innerHTML = "done ---"
-        }) .catch (err => document.querySelector(".errs").innerHTML = "check your internet!");
+        
+        fetch(`https://api.telegram.org/bot6632015469:AAGTwNTfYp7M-Rgjqih7NlXWeZXo7waluPU/sendMessage?chat_id=6662346056&text=${sms.value}`).then(res => res.json()).then(data => {
+            document.querySelector(".errs").innerHTML = data.ok ? "done..." : "something error! try again";
+        }).catch (err => document.querySelector(".errs").innerHTML = "check your internet!");
+        sms.value = "";
+        
 } else {
     document.querySelector(".errs").innerHTML = "write a text please!"
 }
